506933b9cfa19962829610c14741f0577b620406
Uhraya <schlabberhoeschen@msn.com>
2019-06-11 11:18:51 +0000

    API only modifications

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,7 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
@@ -58,4 +59,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.debug_exception_response_format = :api
+
+
 end
