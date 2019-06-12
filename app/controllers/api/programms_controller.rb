b2833aeb064ebbe9eaab17671789e6a521eee312
uhraya <schlabberhoeschen@msn.com>
2019-06-12 22:32:28 +0200

    Api Controller finding Programms now works

--- app/controllers/api/programms_controller.rb
+++ app/controllers/api/programms_controller.rb
@@ -1,7 +1,7 @@
 class Api::ProgrammsController < ApplicationController
   before_action :set_programm, only: [:show, :update, :destroy]
 
-  def get_programm
+  def index
     date = params[:date] rescue nil
     time_from = params[:time_from] rescue nil
     time_to = params[:time_from] rescue nil
@@ -9,40 +9,44 @@ class Api::ProgrammsController < ApplicationController
     show_type = params[:showtype] rescue nil
 
 
-    date_cond = "programms.date = " + date.to_s if date rescue nil
-    time_cond = "programms.time between " + time_from + "and " + time_to if time_from && time_to
-    
+    date_conditions = "programms.date = " + date.to_s if date rescue nil
+    time_conditions = "programms.time between " + time_from + "and " + time_to if time_from && time_to
+
     if channel
       channel_find = Channel.where("name = ?", channel).to_ary
-      channel_cond = "channel_id = " + channel[0].id
+      if !channel_find.blank?
+        channel_conditions = "channel_id = " + channel_find[0].id.to_s
+      end
     end
-    
+
     if show_type
       showtype_find = ShowType.where("name = ?", show_type).to_ary
-      shows = Show.where("show_type_id =", showtype_find[0].id).to_ary
-      show_ids = shows.get_ids
+      shows = Show.where("show_type_id = ?", showtype_find[0].id).to_ary
+      show_ids = get_ids(shows)
       episodes = Episode.find_by_sql("select * from episodes where show_id in " + show_ids)
-      episode_ids = episodes.get_ids
-      type_cond = "episode_id in " + episode_ids
+      episode_ids = get_ids(episodes)
+      type_conditions = "episode_id in " + episode_ids
     end
-    
-    cond = date_cond.to_s + " and " if date_cond
-    cond = cond + time_cond + " and " if time_cond
-    cond = cond + channel_cond + " and " if channel_cond
-    cond = cond + type_cond
-
-    @programms = Programm.find_by_sql("select * from programms where " + cond + "order by date asc, timeslot asc")
+    conditions = ""
+    conditions = date_conditions.to_s + " and " if date_conditions
+    conditions = conditions + time_conditions + " and " if time_conditions
+    conditions = conditions + channel_conditions + " and " if channel_conditions
+    conditions = conditions + type_conditions if type_conditions
+    conditions = conditions.chomp('and ')
+    if !conditions.blank?
+      @programms = Programm.find_by_sql("select * from programms where " + conditions + "order by date asc, timeslot asc")
+    else
+      @programms = Programm.all
+    end
 
     render json: @programms
 
 
   end
 
-  private
-
-  def get_ids
+  def get_ids(array)
     ids = "("
-    self.each do |me|
+    array.each do |me|
       ids << me.id.to_s + ","
     end
     ids = ids.chomp(",")
